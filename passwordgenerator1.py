c409f2d2d43238c8edb85c34ceae29054234935d
PPrrooggrraammeerr <81397961+PPrrooggrraammeerr@users.noreply.github.com>
2021-04-27 20:00:41 -0300

    Add files via upload

---
+++ passwordgenerator1.py
@@ -0,0 +1,57 @@
+
+# coding: UTF-8
+
+# passwordgenerator1.py
+
+import string, random, subprocess, sys
+
+while True:
+
+    subprocess.run (['clear'], shell=True)
+    subprocess.run (['sleep 0.5'], shell=True)
+
+    print ('\n')
+    print ('******************************************************************************')
+    print (r"""
+                         _
+ ____                   /_/_        _         _____         _ _              *
+|  _ \  ___ _ __ ___   ___| |_ _ __(_) ___   |  ___| __ ___(_) |_ __ _ ___
+| | | |/ _ \ '_ ` _ \ / _ \ __| '__| |/ _ \  | |_ | '__/ _ \ | __/ _` / __|  *
+| |_| |  __/ | | | | |  __/ |_| |  | | (_) | |  _|| | |  __/ | || (_| \__ \
+|____/ \___|_| |_| |_|\___|\__|_|  |_|\___/  |_|  |_|  \___|_|\__\__,_|___/  *
+    """)
+    print ('\n******************************************************************************')
+    print ('\n* Copyright of Demétrio Freitas, 2021                                        *')
+    print ('\n* https://github.com/PPrrooggrraammeerr                                      *')
+    print ('\n* https://github.com/PPrrooggrraammeerr/Python/passwordgenerator1.py         *')
+    print ('\n******************************************************************************')
+    print ('\n')
+
+    characters = string.digits
+
+    size = int (input ('Enter the password size: '))
+    subprocess.run (['sleep 0.5'], shell=True)
+    subprocess.run (['clear'], shell=True)
+
+    if size:
+
+        aleatory = random.sample (characters, size)
+
+        result = ''.join (aleatory)
+
+        print (result)
+        subprocess.run (['sleep 2.5'], shell=True)
+        subprocess.run (['clear'], shell=True)
+        break
+
+    else:
+
+        subprocess.run (['clear'], shell=True)
+        subprocess.run (['sleep 0.5'], shell=True)
+        break
+
+while True:
+
+    subprocess.run (['clear'], shell=True)
+    subprocess.run (['sleep 0.5'], shell=True)
+    sys.exit ()
