65eb6bc5156943c15021b250b16d7349c649c429
PPrrooggrraammeerr <81397961+PPrrooggrraammeerr@users.noreply.github.com>
2022-10-06 17:45:43 -0300

    Add files via upload

---
+++ additiontable.py
@@ -0,0 +1,110 @@
+
+# coding: UTF-8
+
+# additiontable.py
+
+import subprocess
+
+while True:
+
+    subprocess.run (['clear'], shell=True)
+    subprocess.run (['sleep 0.5'], shell=True)
+
+    print ('\n')
+    print ('******************************************************************************')
+    print (r"""
+                         _
+ ____                   /_/_        _         _____         _ _              *
+|  _ \  ___ _ __ ___   ___| |_ _ __(_) ___   |  ___| __ ___(_) |_ __ _ ___
+| | | |/ _ \ '_ ` _ \ / _ \ __| '__| |/ _ \  | |_ | '__/ _ \ | __/ _` / __|  *
+| |_| |  __/ | | | | |  __/ |_| |  | | (_) | |  _|| | |  __/ | || (_| \__ \
+|____/ \___|_| |_| |_|\___|\__|_|  |_|\___/  |_|  |_|  \___|_|\__\__,_|___/  *
+
+    """)
+
+    print ('\n******************************************************************************')
+    print ('\n* Copyright of Demétrio Freitas, 2022                                        *')
+    print ('\n* https://github.com/PPrrooggrraammeerr                                      *')
+    print ('\n* https://github.com/PPrrooggrraammeerr/Python/additiontable.py              *')
+    print ('\n******************************************************************************')
+    print ('\n')
+
+    subprocess.run (['sleep 2.5'], shell=True)
+    subprocess.run (['clear'], shell=True)
+    break
+
+while True:
+
+    one = 1
+
+    numbers = range (1, 11)
+
+    print ('\n')
+
+    for number in numbers:
+
+        print (one, '+', number, '=', (one + number))
+
+    print ('\n')
+
+    for number in numbers:
+
+        print (one + one, '+', number, '=', (one + one + number))
+
+    print ('\n')
+
+    for number in numbers:
+
+        print (one + one + one, '+', number, '=', (one + one + one + number))
+
+    print ('\n')
+
+    for number in numbers:
+
+        print (one + one + one + one, '+', number, '=', (one + one + one + one + number))
+
+    print ('\n')
+
+    for number in numbers:
+
+        print (one + one + one + one + one, '+', number, '=', (one + one + one + one + one + number))
+
+    print ('\n')
+
+    for number in numbers:
+
+        print (one + one + one + one + one + one, '+', number, '=', (one + one + one + one + one + one + number))
+
+    print ('\n')
+
+    for number in numbers:
+
+        print (one + one + one + one + one + one + one, '+', number, '=', (one + one + one + one + one + one + one + number))
+
+    print ('\n')
+
+    for number in numbers:
+
+        print (one + one + one + one + one + one + one + one, '+', number, '=', (one + one + one + one + one + one + one + one + number))
+
+    print ('\n')
+
+    for number in numbers:
+
+        print (one + one + one + one + one + one + one + one + one, '+', number, '=', (one + one + one + one + one + one + one + one + one + number))
+
+    print ('\n')
+
+    for number in numbers:
+
+        print (one + one + one + one + one + one + one + one + one + one, '+', number, '=', (one + one + one + one + one + one + one + one + one + one + number))
+
+    print ('\n')
+    break
+
+def finish ():
+
+    exit ()
+
+finish ()
+
