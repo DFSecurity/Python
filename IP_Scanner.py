d4cca5e6670b6dcd15c1b6f5a58abe642fa731ca
Demétrio de Freitas Oliveira <81397961+DFSecurity@users.noreply.github.com>
2023-05-13 18:31:29 -0300

    Add files via upload

--- IP_Scanner.py
+++ IP_Scanner.py
@@ -3,67 +3,46 @@
 
 # IP_Scanner
 
-import ipaddress, os
+import ipaddress, os, time
 
-os.system ('cls')
-user_profile = os.getlogin ()
-IP_or_host = input ('IP or host with range: ')
-generators = ipaddress.ip_network ('{}'.format (IP_or_host))
-print ('\n')
+try:
 
-if not os.path.exists (f'C:\\Users\\{user_profile}\\Desktop\\IPs.txt'):
+    os.system ('cls')
+    user_profile = os.getlogin ()
+    IP_or_host = input ('IP or host with range: ')
+    generators = ipaddress.ip_network ('{}'.format (IP_or_host))
+    print ('\n')
 
     with open (f'C:\\Users\\{user_profile}\\Desktop\\IPs.txt', 'w') as file:
 
         for generator in generators:
 
-            file.write ('{}\n'.format (generator))
+            file.write ('{}\n'.format (generator))            
+
+    with open (f'C:\\Users\\{user_profile}\\Desktop\\IPs.txt', 'r') as file:
+
+        IPs = file.readlines ()
+
+        for IP in IPs:
+
+            IP = IP.split () [0]
+            response = os.popen (f'ping {IP} -n 1').read ()
+
+            if 'Aproximar um n£mero redondo de vezes em milissegundos:' in response:
             
-    with open (f'C:\\Users\\{user_profile}\\Desktop\\IPs.txt', 'r') as file:
+                results = open (f'C:\\Users\\{user_profile}\\Desktop\\IP_Scanner_results.txt', 'a')
+                results.write ('{} is up\n'.format (IP))
+                results.close ()
 
-        IPs = file.readlines ()
+            elif 'Resposta de {}: Host de destino inacess¡vel.'.format (IP) or 'Esgotado o tempo limite do pedido.' in response:
+                
+                print ('{} is down'.format (IP))
 
-        for IP in IPs:
+except KeyboardInterrupt:
 
-            IP = IP.split () [0]
-            response = os.popen (f'ping {IP} -n 1').read ()
-
-            try:
-
-                if 'Aproximar um n£mero redondo de vezes em milissegundos:' in response:
-        
-                    print ('\033[32m{} is up'.format (IP))
-
-                elif 'Resposta de {}: Host de destino inacess¡vel.'.format (IP) or 'Esgotado o tempo limite do pedido.' in response:
-        
-                    print ('\033[31m{} is down'.format (IP))
-
-            except:
-
-            	pass
-		    
-elif os.path.exists (f'C:\\Users\\{user_profile}\\Desktop\\IPs.txt'):
-
-    with open (f'C:\\Users\\{user_profile}\\Desktop\\IPs.txt', 'r') as file:
-
-        IPs = file.readlines ()
-
-        for IP in IPs:
-
-            IP = IP.split () [0]
-            response = os.popen (f'ping {IP} -n 1').read ()
-
-            try:
-
-                if 'Aproximar um n£mero redondo de vezes em milissegundos:' in response:
-        
-                    print ('\033[32m{} is up'.format (IP))
-
-                elif 'Resposta de {}: Host de destino inacess¡vel.'.format (IP) or 'Esgotado o tempo limite do pedido.' in response:
-        
-                    print ('\033[31m{} is down'.format (IP))
-
-            except:
-
-            	pass
+    os.system ('cls')
+    os.remove (f'C:\\Users\\{user_profile}\\Desktop\\IPs.txt')
+    time.sleep (0.5)
+    os.system ('taskkill /f /im python3.10.exe > nul')
+    
 
