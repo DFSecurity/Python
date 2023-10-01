fd170fcf047e8c51611462de8ddefe5e304f0b3b
Demétrio de Freitas Oliveira <81397961+DFSecurity@users.noreply.github.com>
2023-10-01 16:18:01 -0300

    Update ordem_crescente.py

--- ordem_crescente.py
+++ ordem_crescente.py
@@ -1,65 +1,21 @@
-
 # coding: UTF-8
 
 # ordem_crescente.py
 
+# O script ordem_crescente.py solicita que o usuário insira três valores diferentes e os imprima em ordem crescente.
+
 import os, time
 
+os.system('clear')
+
+values = []
+values.append(int(input('value1: ')))
+values.append(int(input('value2: ')))
+values.append(int(input('value3: ')))
+
+sorted_values = sorted(values)
+
 os.system ('clear')
-
-value1 = int (input ('value1: '))
-value2 = int (input ('value2: '))
-value3 = int (input ('value3: '))
-
-if value1 < value2:
-
-    if value2 < value3:
-
-        os.system ('clear')
-        print (value1, value2, value3)
-        time.sleep (2.5)
-        os.system ('clear')
-
-    else:
-
-        if value1 > value3:
-
-            os.system ('clear')
-            print (value3, value1, value2)
-            time.sleep (2.5)
-            os.system ('clear')
-
-        else:
-
-            os.system ('clear')
-            print (value1, value3, value2)
-            time.sleep (2.5)
-            os.system ('clear')
-
-else:
-
-    if value2 > value3:
-
-        os.system ('clear')
-        print (value3, value2, value1)
-        time.sleep (2.5)
-        os.system ('clear')
-
-    else:
-
-        if value3 > value2:
-
-            os.system ('clear')
-            print (value2, value3, value1)
-            time.sleep (2.5)
-            os.system ('clear')
-
-        else:
-
-            os.system ('clear')
-            print (value2, value3, value1)
-            time.sleep (2.5)
-            os.system ('clear')
-
-exit ()
-
+print (sorted_values [0], sorted_values [1], sorted_values [2])
+time.sleep (3.5)
+os.system ('clear')
