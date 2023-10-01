144f9176b151e88446189ae3fbac97ee72043539
Demétrio de Freitas Oliveira <81397961+DFSecurity@users.noreply.github.com>
2023-10-01 13:00:19 -0300

    Update IPaddressoptions.py

--- IPaddressoptions.py
+++ IPaddressoptions.py
@@ -1,111 +1,92 @@
 
 # coding: UTF-8
 
-# IPaddressoptions.py
+# IP_address_options.py
 
-import socket, requests, subprocess, sys, os, time
+# O script IP_address_options.py é uma ferramenta de linha de comando que fornece ao usuário uma interface simples para verificar diferentes endereços IP associados ao sistema ou à conexão de rede atual.
 
-while True:
+import socket
+import requests
+import os
+import sys
+import time
 
-    subprocess.run (['clear'], shell=True)
-    subprocess.run (['sleep 0.5'], shell=True)
+def clear_screen ():
 
-    print ('\n')
-    print ('******************************************************************************')
-    print (r"""
+    os.system ('cls' if os.name == 'nt' else 'clear')
+
+def show_banner ():
+
+    clear_screen ()
+
+    print('\n')
+    print('******************************************************************************')
+    print(r"""
                          _
- ____                   /_/_        _         _____         _ _              *
+ ____                   /_/_        _         _____         _ _
 |  _ \  ___ _ __ ___   ___| |_ _ __(_) ___   |  ___| __ ___(_) |_ __ _ ___
-| | | |/ _ \ '_ ` _ \ / _ \ __| '__| |/ _ \  | |_ | '__/ _ \ | __/ _` / __|  *
+| | | |/ _ \ '_ ` _ \ / _ \ __| '__| |/ _ \  | |_ | '__/ _ \ | __/ _` / __|
 | |_| |  __/ | | | | |  __/ |_| |  | | (_) | |  _|| | |  __/ | || (_| \__ \
-|____/ \___|_| |_| |_|\___|\__|_|  |_|\___/  |_|  |_|  \___|_|\__\__,_|___/  *
+|____/ \___|_| |_| |_|\___|\__|_|  |_|\___/  |_|  |_|  \___|_|\__\__,_|___/
 
     """)
 
-    print ('\n******************************************************************************')
-    print ('\n* Copyright of Demétrio Freitas, 2021                                        *')
-    print ('\n* https://github.com/PPrrooggrraammeerr                                      *')
-    print ('\n* https://github.com/PPrrooggrraammeerr/Python/IPaddressoptions.py           *')
-    print ('\n******************************************************************************')
-    print ('\n')
+    print('\n******************************************************************************')
+    print('\n* Copyright of Demétrio Freitas, 2021                                        *')
+    print('\n* https://github.com/DFSecurity                                              *')
+    print('\n* https://github.com/DFSecurity/Python/IP_address_options.py                 *')
+    print('\n******************************************************************************')
+    print('\n')
 
-    subprocess.run (['sleep 2.5'], shell=True)
-    subprocess.run (['clear'], shell=True)
-    break
+    time.sleep(3.5)
+    clear_screen()
 
-while True:
+def main ():
 
-    def main ():
+    while True:
 
-        try:
+        clear_screen ()
 
-            print ('\n')
-            print ('\n***********************')
-            print ('\n*  1 - IP internal    *')
-            print ('\n*  2 - IP external    *')
-            print ('\n*  3 - IP localhost   *')
-            print ('\n*  4 - Exit program   *')
-            print ('\n***********************')
-            print ('\n')
+        print ('\n')
+        print ("""    ===========================
+   *                           *
+   *      1 - IP internal      *
+   *      2 - IP external      *
+   *      3 - IP localhost     *
+   *      4 - Exit program     *
+   *                           *
+    =========================== """)
+        print ('\n')
 
-            subprocess.run (['sleep 0.5'], shell=True)
-            Type_the_option = int (input ("What's my IP? Type the option: "))
+        option = input ("What's my IP? Type the option: ")
 
-            if Type_the_option == 1:
+        if option == '1':
 
-                subprocess.run (['clear'], shell=True)
+            client = socket.socket (socket.AF_INET, socket.SOCK_DGRAM)
+            client.connect (('8.8.8.8', 80))
+            result = client.getsockname () [0]
+            print (f"What's my IP internal: {result}")
+            time.sleep (3.5)
 
-                client = socket.socket (socket.AF_INET, socket.SOCK_DGRAM)
-                client.connect (('8.8.8.8', 80))
-                result = client.getsockname () [0]
+        elif option == '2':
 
-                print ("What's my IP internal: {}".format (result))
-                subprocess.run (['sleep 2.5'], shell=True)
-                subprocess.run (['clear'], shell=True)
+            IP_external = requests.get ('https://api.ipify.org/').text
+            print (f"What's my IP external: {IP_external}")
+            time.sleep (3.5)
 
-            elif Type_the_option == 2:
+        elif option == '3':
 
-                subprocess.run (['clear'], shell=True)
+            hostname = socket.gethostname ()
+            localhost = socket.gethostbyname (hostname)
+            print (f"What's my IP localhost: {localhost}")
+            time.sleep (3.5)
 
-                IP_external = requests.get('https://api.ipify.org/').text
+        elif option == '4':
 
-                print (f"What's my IP external: {IP_external}")
-                subprocess.run (['sleep 2.5'], shell=True)
-                subprocess.run (['clear'], shell=True)
-
-            elif Type_the_option == 3:
-
-                subprocess.run (['clear'], shell=True)
-
-                hostname = socket.gethostname()
-                localhost = socket.gethostbyname(hostname)
-
-                print ("What's my IP localhost: {}".format (localhost))
-                subprocess.run (['sleep 2.5'], shell=True)
-                subprocess.run (['clear'], shell=True)
-
-            elif Type_the_option == 4:
-
-                os.system ('clear')
-                time.sleep (0.5)
-                sys.exit ()
-                exit ()
-
-        except KeyboardInterrupt:
-
-            subprocess.run (['clear'], shell=True)
-            time.sleep (0.5)
             sys.exit ()
-            exit ()
 
-    if __name__ == '__main__':
 
-        main ()
+if __name__ == '__main__':
 
-while True:
-
-    os.system ('clear')
-    time.sleep (0.5)
-    sys.exit ()
-    exit ()
-
+    show_banner ()
+    main ()
